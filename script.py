4e4f1e293ef4c67d8d0b611a36224009cd0206fa
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-05-20 22:30:39 -0500

    init commit | init framework list

---
+++ script.py
@@ -0,0 +1,13 @@
+front_end_framework = [
+    "React",
+    "NextJS",
+    "Vue",
+    "Nuxt",
+    "SvelteKit",
+    "Qwik",
+    "QwikCity",
+    "Solid",
+    "SolidStart",
+]
+# meta_framework = ['NextJS', 'Nuxt', 'SvelteKit', 'QwikCity', 'SolidStart']
+backend_end_framework = ["Express/Prisma/PostgreSQL", "Django REST Framework"]
